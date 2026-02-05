946387837eecbcf49d801c81e7937ea6df5b5848
Tarun Goel <tarungoel1823@gmail.com>
2026-02-05 15:04:03 +0530

    added sliding sidebar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,40 @@
-$(document).ready(function() {
-    $("#utif").click(function() {
-        $(".assign-dep").show();
-    });
-    $("#dif").click(function() {
-        $(".assign-dep").hide();
-    });
+$(document).ready(function () {
+  $("#utif").click(function () {
+    $(".assign-dep").show();
+  });
+  $("#dif").click(function () {
+    $(".assign-dep").hide();
+  });
+
+  if (window.innerWidth < 768) {
+    $("body").removeClass("sidebar-open");
+    $("#sidebarClose").hide();
+    $("#sidebarOpen").show();
+  } else {
+    $("body").removeClass("sidebar-collapsed");
+    $("#sidebarOpen").hide();
+    $("#sidebarClose").show();
+  }
+
+  $("#sidebarClose").on("click", function () {
+    if (window.innerWidth < 768) {
+      $("body").removeClass("sidebar-open");
+    } else {
+      $("body").addClass("sidebar-collapsed");
+    }
+
+    $("#sidebarClose").hide();
+    $("#sidebarOpen").show();
+  });
+
+  $("#sidebarOpen").on("click", function () {
+    if (window.innerWidth < 768) {
+      $("body").addClass("sidebar-open");
+    } else {
+      $("body").removeClass("sidebar-collapsed");
+    }
+
+    $("#sidebarOpen").hide();
+    $("#sidebarClose").show();
+  });
 });
